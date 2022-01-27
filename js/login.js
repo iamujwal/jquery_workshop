b22bfb88f1270d57f052302b11ece43c696ce0e1
Ujwal <ujwals@cybage.com>
2022-01-27 18:56:02 +0530

    submitted jQuery_workshop

---
+++ js/login.js
@@ -0,0 +1,28 @@
+$(document).ready(
+    ()=>{
+        $('input[type="button"]').click(
+            ()=>{
+                var username=$('input[type="text"]').val();
+                var password=$('input[type="password"]').val();
+                if(username=='' && password==''){
+                    alert("username and password is not entered");
+                }
+                else{
+                    $.ajax({
+                        url:"http://localhost:3000/employees", 
+                        type:'GET',
+                        data:{
+                            "username":username,
+                            "password":password,
+                        },
+                        success:(x)=>{
+                            alert(username+"Welcome..! logged in successful.." );
+                            console.log(x);
+                           window.location="http://127.0.0.1:5500/html/registration.html"
+                        }
+                    })
+                }
+            }
+        )
+    }
+)
